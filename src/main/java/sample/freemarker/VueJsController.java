87b0ee6fe33f1bd26881eddf757d190dcb44e8ef
xiezhonggui <1573623031@qq.com>
2017-05-26 09:48:05 +0800

    lboss vuejs

---
+++ src/main/java/sample/freemarker/VueJsController.java
@@ -0,0 +1,15 @@
+package sample.freemarker;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by xiezhonggui on 2017/5/26.
+ */
+@Controller
+public class VueJsController {
+    @GetMapping("/vueJs01")
+    public String vueJs01() {
+        return "vueJs01";
+    }
+}
